17a0f24992c0341e8bec190482b2ae6d6395dc0b
a22007560 <a22007560@alunos.ulht.pt>
2023-02-16 23:49:06 +0000

    Basic dart app

---
+++ bin/cm_ex1.dart
@@ -0,0 +1,6 @@
+import 'package:cm_ex1/cm_ex1.dart' as cm_ex1;
+
+void main(List<String> arguments) {
+  var anoLetivo = "2019/2020";
+  print("Olá Computação Móvel $anoLetivo");
+}
---
+++ test/cm_ex1_test.dart
@@ -0,0 +1,8 @@
+import 'package:cm_ex1/cm_ex1.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
